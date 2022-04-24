e7c3883de26680a4e218e5ca29ca6c2fc01d11c7
Zechariah Guadiana <zechariah.guadiana@gmail.com>
2022-04-24 04:45:10 -0500

    Practice using static keywords

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{ TempuratureConversions.FahrenheitToCelsius(100)}");
+            Console.WriteLine($"{ TempuratureConversions.CelsiusToFahrenheit(37.77777777777778)}");
+        }
+    }
+}
---
+++ TempuratureConversions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal class TempuratureConversions
+    {
+        public static double FahrenheitToCelsius(double Fahrenheit) {
+            var celsius = (Fahrenheit - 32) * 5 / 9;
+            return celsius;
+        }
+        public static double CelsiusToFahrenheit(double Celsius)
+        {
+            var fahrenheit = (Celsius*9/5)+32;
+            return fahrenheit;
+        }
+    }
+}
